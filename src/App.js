c65d8dddc5a935da6eb0bd477b6daccb0613800f
joaovsa97 <joaos.azevedo10@gmail.com>
2023-01-11 12:36:12 -0300

    add change screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+//Css
 import "./App.css";
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//Data
+import { wordsList } from "./data/word";
+
+//Components
 import StartScreen from "./componets/startPage/StartScreen";
+import GameScreen from "./componets/gameScreen/GameScreen";
+import GameOver from "./componets/GameOver/GameOver";
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'},
+]
 
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const[words] = useState(wordsList)
+
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  const exitGame = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-      <StartScreen />
+      <div>
+        {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+        {gameStage === 'game' && <GameScreen />}
+        {gameStage === 'end' && <GameOver />}
+      </div>
     </div>
   );
 }
---
+++ src/componets/gameScreen/GameScreen.js
@@ -0,0 +1,11 @@
+import "./GameScreen.css";
+
+const GameScreen = () => {
+  return (
+    <div>
+      <h1>GameScreen</h1>
+    </div>
+  );
+};
+
+export default GameScreen;
